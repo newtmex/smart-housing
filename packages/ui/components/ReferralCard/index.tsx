a49128d87d16e9997045e1c771189f40951ede86
newtmex <brightomex@gmail.com>
2024-08-09 08:01:25 +0100

    UI improvement

--- packages/ui/components/ReferralCard/index.tsx
+++ packages/ui/components/ReferralCard/index.tsx
@@ -31,7 +31,7 @@ export default function ReferralCard() {
       <div className="cta-content extra-padded">
         <div className="highlight-header">Bonus</div>
         <h5 className="cta-header">Invite your friends and enjoy more rewards with referrals</h5>
-        <span className="badge bg-success">
+        <span className="badge bg-success" style={{ width: "100%", overflowX: "scroll" }}>
           <span id="ref_link">{joinLink}</span>
         </span>
         <CopyButton className="text-white ml-2" text={joinLink} />
