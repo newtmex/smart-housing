2e9a5813adf37f9c5e5ad6be320bbe0824366fe6
newtmex <brightomex@gmail.com>
2024-08-26 11:10:09 +0100

    verify config

--- packages/backend/hardhat.config.ts
+++ packages/backend/hardhat.config.ts
@@ -12,14 +12,10 @@ import "hardhat-deploy-ethers";
 import "./scripts/deployHousingProject";
 import "./scripts/feedSmartHousing";
 
-// If not set, it uses ours Alchemy's default API key.
-// You can get your own at https://dashboard.alchemyapi.io
-const providerApiKey = process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 // If not set, it uses the hardhat account 0 private key.
 const deployerPrivateKey =
   process.env.DEPLOYER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-// If not set, it uses ours Etherscan default API key.
-const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
+const nodeRealApiKey = process.env.NODEREAL_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -53,13 +49,13 @@ const config: HardhatUserConfig = {
   },
   // configuration for harhdat-verify plugin
   etherscan: {
-    apiKey: { opbnb: `${etherscanApiKey}` },
+    apiKey: { opbnb: `${nodeRealApiKey}` },
     customChains: [
       {
         network: "opbnb",
         chainId: 5611, // Replace with the correct chainId for the "opbnb" network
         urls: {
-          apiURL: `https://open-platform.nodereal.io/${etherscanApiKey}/op-bnb-testnet/contract/`,
+          apiURL: `https://open-platform.nodereal.io/${nodeRealApiKey}/op-bnb-testnet/contract/`,
           browserURL: "https://testnet.opbnbscan.com/",
         },
       },
